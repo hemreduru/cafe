d4fec204d04c23088bb864d1e423a3ddaa00f631
emre <hemreduru@gmail.com>
2025-01-06 21:01:16 +0200

    remove bistro

--- resources/views/menu-layout/components/loader.blade.php
+++
@@ -1,7 +0,0 @@
-<!--Loader-->
-<div class="loader">
-    <div class="cssload-container">
-        <div class="cssload-circle"></div>
-        <div class="cssload-circle"></div>
-    </div>
-</div>
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\QRMenuController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/menu', [QRMenuController::class, 'index'])->name('qrmenu');
 Route::middleware('guest')->group(function () {
     Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
     Route::post('/login', [LoginController::class, 'login']);
